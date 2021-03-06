c500f011c75f304f66b9771fc58da67f8c205084
justin kringstad <78430579+SithlordSparky@users.noreply.github.com>
2021-03-06 16:21:17 -0600

    FIREBASE SETUP

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,12 @@
-export const App = () => <>Hello from PortEXE</>;
+import { Switch, Route } from 'react-router-dom';
+import { Login, Signup, Chat } from 'components';
+
+export const App = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Chat} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  );
+};
