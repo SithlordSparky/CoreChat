8ed1ca027a45d6b1093b6a8931c1db61eebad738
justin kringstad <78430579+SithlordSparky@users.noreply.github.com>
2021-03-06 10:01:07 -0600

    test

--- core-main/src/App.js
+++ core-main/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Core</h2>
+        <p>testing</p>
       </header>
     </div>
   );
