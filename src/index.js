ea80bfdeec37c81ec9194bccffa0bf280b4ec576
justin kringstad <78430579+SithlordSparky@users.noreply.github.com>
2021-03-07 19:22:13 -0600

    Complete! All Done!

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 import { App } from 'components';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// vercel env ls
+// to lookup Env keys
+
 ReactDOM.render(
+  // Remove StrictMode to remove warnings.
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
